46d32a6648d2e643816e97c85a3a6f2d688b06bd
tankangliang98@gmail.com <tankangliang98@gmail.com>
2020-05-18 15:33:03 +0800

    Added structure to User Resolver

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,10 @@
 import mongoose, { Schema, Document } from "mongoose";
 import { ProfileType, GroupType } from "./";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// Constant for number of salt rounds
+const saltRounds = 10;
 
 //Interface for user type without mongoDB
 export interface RawUserType {
@@ -23,11 +28,20 @@ export interface RawUserType {
   };
 }
 
+//Interface for token return type
+export interface TokenType {
+  token: string;
+}
+
 //Interface for user type with mongoDB
 export interface UserType extends Document, RawUserType {
   comparePassword(password: string): boolean;
+  tokenize(): TokenType;
 }
 
+/**
+ * User Schema type for mongoose
+ */
 const userSchema: Schema = new mongoose.Schema({
   username: String,
   firstName: String,
@@ -57,10 +71,67 @@ const userSchema: Schema = new mongoose.Schema({
   },
 });
 
+/**
+ * Ensures that password is not returned when converting to JSON
+ */
 userSchema.set("toJSON", {
   transform: (_, returnedObject) => {
     delete returnedObject.password;
   },
 });
+
+/**
+ * Pre-hook in Mongoose that modifies input on save
+ * Sets default dateJoined to now and isVerified to false
+ * Hashes password if modified
+ */
+userSchema.pre("save", async function (next: mongoose.HookNextFunction) {
+  const user = this;
+
+  if (!user.get("dateJoined")) user.set("dateJoined", new Date());
+  if (!user.get("isVerified")) user.set("isVerified", false);
+  if (!user.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(user.get("password"), salt);
+    user.set("password", hash);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+
+  next();
+});
+
+/**
+ * Defines a method in the User Schema to compare a password
+ */
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+) {
+  const currentUser = this;
+  try {
+    const match: boolean = await bcrypt.compare(
+      candidatePassword,
+      currentUser.password
+    );
+    return match;
+  } catch (err) {
+    throw err;
+  }
+};
+
+/**
+ *  Defines a method in the User Schema to tokenize profile
+ */
+userSchema.methods.tokenize = function () {
+  const user = this.toJSON();
+  user.id = user._id;
+  const secret = process.env.TOKEN_SECRET;
+  return {
+    token: jwt.sign(user, <jwt.Secret>secret),
+  };
+};
 
 export const User = mongoose.model<UserType>("User", userSchema);
--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,12 +1,94 @@
 // resolverMap.ts
 import { IResolvers } from "graphql-tools";
+import { User, UserType, TokenType } from "../models";
 
-const defaultResolver: IResolvers = {
-  Query: {
-    helloWorld(_: void, args: void): string {
-      return "👋 Hello world! 👋";
+const userResolver: IResolvers = {
+  Mutation: {
+    /**
+     * Creates a user with specified arguments.
+     * Generates user profile and links their documents together
+     * Generates token to be returned to user for authentication
+     *
+     * VALIDATION: Unique Username, Unique Email
+     */
+    createUser: async (
+      root: void,
+      args: {
+        username: string;
+        password: string;
+        confirmPassword: string;
+        firstName: string;
+        lastName: string;
+        email: string;
+        gender: string;
+        birthday: Date;
+      }
+    ): Promise<TokenType> => {
+      //TODO
+      return { token: "TODO" };
+    },
+    /**
+     * Handles user log in
+     *
+     * Generates token to be returned to user for authentication
+     */
+    loginUser: async (
+      root: void,
+      args: {
+        username: string;
+        password: string;
+      }
+    ): Promise<TokenType> => {
+      //TODO
+      return { token: "TODO" };
+    },
+    /**
+     * Checks if both username and email match in the document.
+     *
+     * Sends an email with a randomly generated password to the user.
+     */
+    forgetPassword: async (
+      root: void,
+      args: {
+        username: string;
+        email: string;
+      }
+    ): Promise<boolean> => {
+      //TODO
+      return true;
+    },
+    /**
+     * Confirms that previous password matches.
+     *
+     * Updates password to be new password. Make sure to use .save() for mongoose
+     * pre-hook to take effect.
+     */
+    resetPassword: async (
+      root: void,
+      args: {
+        originalPassword: string;
+        newPassword: string;
+        confirmNewPassword: string;
+      }
+    ): Promise<boolean> => {
+      //TODO
+      return true;
+    },
+    /**
+     * Deletes a user based on username.
+     * Deletes associated user profile as well.
+     * Mostly for testing.
+     */
+    deleteUser: async (
+      root: void,
+      args: {
+        username: string;
+      }
+    ): Promise<boolean> => {
+      //TODO
+      return true;
     },
   },
 };
 
-export default defaultResolver;
+export default userResolver;
